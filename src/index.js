d8f5c616ede5a659148a3b04982296bf0ccecfed
vitaliy-kro <krochakv6@gmail.com>
2022-09-28 12:44:41 +0300

    add import localstorage functions  to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './js/api-trending';
 import './js/api-search-movies';
 import './js/api-full-info-movie';
+import {
+  addFilmToLocalStorage,
+  getFilmsFromLocalStorage,
+  deleteFilmFromLocaleStorage,
+} from './js/localstorage';
