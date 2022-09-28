54753aae7a3e0c15068336e6c7dc8d470b656b83
OlyaNegrych <olyanegrych@gmail.com>
2022-09-28 22:16:59 +0300

    added pagination markup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import './js/api-trending';
 import './js/api-search-movies';
 import './js/api-full-info-movie';
 import './js/change-page';
-import {
-  addFilmToLocalStorage,
-  getFilmsFromLocalStorage,
-  deleteFilmFromLocaleStorage,
-} from './js/localstorage';
-
-import './js/pagination'
+import './js/paginationFn'
