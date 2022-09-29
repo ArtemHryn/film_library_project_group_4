055d816b3e8b40c6e0acc070578cb0824be7f21a
ArtemHryn <grynbest@gmail.com>
2022-09-29 22:21:41 +0300

    finished search

--- src/js/api-search-movies.js
+++ src/js/api-search-movies.js
@@ -1,37 +1,46 @@
-import { API_KEY } from "./API_KEY"
+import { API_KEY } from './API_KEY';
 import axios from 'axios';
-import { BASE_URL } from './api-trending'
+import { BASE_URL } from './api-trending';
 
 export class MoviesSearchAPIService {
-    constructor() {
-        this.searchQuery = ''; //'Titanic' ------> для тестування(string)
-        this.page = 1;
-    }
-    
-     async fetchMovies() {
-         try {
-      const response = await axios.get(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.searchQuery}&page=${this.page}`);
-             this.incrementPage();
-             console.log(response.data)
-      return response.data.results;
+  constructor() {
+    this.searchQuery = ''; //'Titanic' ------> для тестування(string)
+    this.page = 1;
+    this.listOfFilms = {};
+  }
+
+  async fetchMovies() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.searchQuery}&page=${this.page}`
+      );
+
+      return response.data;
     } catch (error) {
-      throw new Error(`Oops, something went wrong`)
+      throw new Error(`Oops, something went wrong`);
     }
-    }
-    
-    resetPage() {
+  }
+
+  resetPage() {
     this.page = 1;
   }
 
   incrementPage() {
     this.page += 1;
-    }
-    
-     get query() {
+  }
+
+  get query() {
     return this.searchQuery;
   }
   set query(newQuery) {
     this.searchQuery = newQuery;
+  }
+  get films() {
+    return this.listOfFilms;
+  }
+
+  set films(newFilm) {
+    this.listOfFilms = newFilm;
   }
 }
 
