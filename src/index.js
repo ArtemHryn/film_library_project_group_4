a9d76a32af256ebbd0c4c4b69c7f7276061f6474
Alex Supotnytskyi <kedred725@gmail.com>
2022-09-29 17:13:00 -0400

    add pre-load page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import './js/main-page-pre-load';
 import './js/firebase/service';
 import './js/api-trending';
 import './js/api-search-movies';
 import './js/api-full-info-movie';
 import './js/change-page';
-import './js/paginationFn'
-import "./js/user/login";
+import './js/paginationFn';
+import './js/user/login';
 import './js/pagination';
 import './js/addingFilmToWeb';
