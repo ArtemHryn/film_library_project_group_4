3b01b334f12fcf60034f15916c95c5d3ff6b0407
Mykola Harasym <henkmoody7@gmail.com>
2022-10-01 17:31:11 +0300

    fixed

--- src/js/addBackdrop.js
+++ src/js/addBackdrop.js
@@ -1,10 +1,12 @@
 const backdrop = document.querySelector('.backdrop');
 
 export function changeBackdrop(img) {
-  const backdropImage = img;
-  if (backdropImage !== null) {
-    backdrop.style.backgroundImage = `url('https://image.tmdb.org/t/p/original/${backdropImage}')`;
+  if (img !== null) {
+    backdrop.style.backgroundImage = `url('https://image.tmdb.org/t/p/original/${img}')`;
     backdrop.style.backgroundSize = 'cover';
     backdrop.style.backgroundPosition = 'center';
+  } else {
+    backdrop.style.backgroundImage =
+      'https://static.vecteezy.com/system/resources/previews/003/582/701/original/coming-soon-background-illustration-template-design-free-vector.jpg';
   }
 }
