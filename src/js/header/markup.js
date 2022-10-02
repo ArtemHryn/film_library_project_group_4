93d26ba081a9f2883dc84d4319a65cdc215f6b8e
ancorframe <yura.pundiak@gmail.com>
2022-10-02 22:40:15 +0300

    fix svg area label

--- src/js/header/markup.js
+++ src/js/header/markup.js
@@ -2,8 +2,8 @@ import { refs } from '../refs/index';
 
 export function markupLibraryBtn() {
   refs.container.innerHTML = `<div class='btn-flex'>
-  <button type="button" class="lib-btn" data-value="wached">Watched</button>
-    <button type="button" class="lib-btn" data-value="queue">queue</button>
+  <button type="button" class="lib-btn" data-value="wached" aria-label="Watched">Watched</button>
+    <button type="button" class="lib-btn" data-value="queue" aria-label="Queue">queue</button>
     </div>`;
 }
 
@@ -15,11 +15,10 @@ export function markupSearchQuery() {
         autocomplete="off"
         placeholder="Movie search"
         class="input-search"
+        aria-label="Input search"
       />
-      <button type="submit" class="submit-btn">
-        <svg width="15" height="15" class='search-svg'>
-          <use href="./images/sprite.svg#icon-search"></use>
-        </svg>
+      <button type="submit" class="submit-btn" aria-label="Search">
+        <svg width="12" height="12" fill="none"><path d="M5.5 9.5a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM10.5 10.5 8.325 8.325" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/></svg>
       </button>
     </form>
     <p class="search-error visually-hidden">
