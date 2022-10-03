611ea1340f4fc6b98a410c289ed9bc3e6e741c07
Mykola Harasym <henkmoody7@gmail.com>
2022-10-03 14:49:53 +0300

    save theme to local

--- src/js/togle-theme.js
+++ src/js/togle-theme.js
@@ -2,15 +2,38 @@ import { addClass, removeClass } from '../js/utils/changeclass';
 
 const bodyRef = document.querySelector('body');
 const toggleRef = document.querySelector('.switch');
-
 toggleRef.addEventListener('change', onChange);
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+getThemeFromLocal();
+addClass(bodyRef, Theme.LIGHT);
+
 function onChange() {
-  if (bodyRef.classList.contains('dark-theme')) {
-    removeClass(bodyRef, 'dark-theme');
-    addClass(bodyRef, 'light-theme');
-  } else {
-    removeClass(bodyRef, 'light-theme');
-    addClass(bodyRef, 'dark-theme');
+  if (bodyRef.classList.contains(Theme.DARK)) {
+    removeClass(bodyRef, Theme.DARK);
+    addClass(bodyRef, Theme.LIGHT);
+    setThemeToLocal();
+  } else if (document.body.classList.contains(Theme.LIGHT)) {
+    removeClass(bodyRef, Theme.LIGHT);
+    addClass(bodyRef, Theme.DARK);
+    setThemeToLocal();
+  }
+}
+
+function getThemeFromLocal() {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    addClass(bodyRef, Theme.DARK);
+  }
+}
+
+function setThemeToLocal() {
+  if (bodyRef.classList.contains(Theme.LIGHT)) {
+    localStorage.setItem('theme', Theme.LIGHT);
+  } else if (bodyRef.classList.contains(Theme.DARK)) {
+    localStorage.setItem('theme', Theme.DARK);
   }
 }
