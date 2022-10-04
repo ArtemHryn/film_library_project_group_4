d4bd928387921f31ba50014588ef2a293a56240a
Artem <grynbest@gmail.com>
2022-10-04 12:49:06 +0300

    Update togleTheme.js

--- src/js/togleTheme.js
+++ src/js/togleTheme.js
@@ -1,4 +1,4 @@
-import { addClass, removeClass } from './utils/changeClass';
+import { addClass, removeClass } from './utils/changeСlass';
 import { setThemeToLocal } from './localstorage';
 
 const Theme = {
